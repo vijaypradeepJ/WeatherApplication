e21250011a0b0b431bf16a61460bf6515cb8ea88
Vijay pradeep <vijaypradeep3568@gmail.com>
2025-01-07 22:30:20 +0530

    add styles for searchEngine Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import "../src/Components/Style.css"
 import SearchEngine from './Components/SearchEngine';
 
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <SearchEngine/>
     </div>
   )
--- src/Components/SearchEngine.js
+++ src/Components/SearchEngine.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
+import "./Style.css";
 
 function SearchEngine() {
   return (
-    <div className='searchengine'>
+    <div className='SearchEngine'>
         <input type="text"
         className='city-serach'
         placeholder='Enter the city name' 
